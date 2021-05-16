f7a518233dd2736f564330c4a68b99c4e6a25a83
Miguel Ángel Correa Manrique <miguelmque@Miguels-MacBook-Air.local>
2021-05-16 15:43:44 -0500

    interpolation class

---
+++ interpolML/interpole.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+class Interpolation:
+
+    def __init__(self, data : pd.DataFrame):
+
+        self.data = data
+
+
+
+    def newton_interpolation(self):
+        return
+
+
+    def rapson_interpolation(self):
+        return
