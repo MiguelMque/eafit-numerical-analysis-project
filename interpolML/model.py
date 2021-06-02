b38f0d497c88d96a04939c1057ce6df6f955e3ba
Miguel Ángel Correa Manrique <miguelmque@Miguels-MacBook-Air.local>
2021-06-01 21:02:30 -0500

    adding models

--- interpolML/model.py
+++ interpolML/model.py
@@ -4,8 +4,13 @@ from keras.layers import Dense, LSTM
 
 
 class Model:
-    import numpy as np
-    def __init__(self, data : pd.DataFrame):
-        self.data = data
-        
 
+    def __init__(self, name: str, model):
+        self.name = name
+        self.model = model
+
+
+    def fit(self, train_dataset : pd.DataFrame):
+        "Performs model training with standard settings"
+
+        self.model.fit()
